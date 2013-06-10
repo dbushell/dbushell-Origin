e11fb44b0c200eb697c3eb5266b986868a9d4198
David Bushell <david@dbushell.com>
2013-06-10 12:07:34 +0100

    fix rasterization

--- tasks/rasterize.js
+++ tasks/rasterize.js
@@ -1,50 +1,104 @@
 var fs = require("fs");
 
-// var inputFile = phantom.args[0],
-//     outputFile = phantom.args[1];
+var styles = {
 
-var svgdatauri = "data:image/svg+xml;base64,",
-    complete = 0,
-    files = phantom.args[0],
-    page = require("webpage").create();
+  'bold'          : ['\x1B[1m',  '\x1B[22m'],
+  'italic'        : ['\x1B[3m',  '\x1B[23m'],
+  'underline'     : ['\x1B[4m',  '\x1B[24m'],
+  'inverse'       : ['\x1B[7m',  '\x1B[27m'],
+  'strikethrough' : ['\x1B[9m',  '\x1B[29m'],
 
-var onComplete = function()
-{
-    if (++complete >= files.length)
-    {
-        phantom.exit(false);
-        process.exit(0);
-    } else {
-        next();
-    }
+  'white'     : ['\x1B[37m', '\x1B[39m'],
+  'grey'      : ['\x1B[90m', '\x1B[39m'],
+  'black'     : ['\x1B[30m', '\x1B[39m'],
+
+  'blue'      : ['\x1B[34m', '\x1B[39m'],
+  'cyan'      : ['\x1B[36m', '\x1B[39m'],
+  'green'     : ['\x1B[32m', '\x1B[39m'],
+  'magenta'   : ['\x1B[35m', '\x1B[39m'],
+  'red'       : ['\x1B[31m', '\x1B[39m'],
+  'yellow'    : ['\x1B[33m', '\x1B[39m']
+
 };
 
-var next = function()
+var style = function(str, format)
 {
-    var inputFile  = files[complete],
-        outputFile = inputFile.replace(/\.svg$/i, ".png");
+    return styles[format][0] + str + styles[format][1];
+};
 
-    var svgdata = fs.read(inputFile) || "";
-
-    // get svg element's dimensions so we can set the viewport dims later
-    var frag = window.document.createElement("div");
-    frag.innerHTML = svgdata;
-    var svg = frag.querySelector("svg");
-    var width = svg.getAttribute("width");
-    var height = svg.getAttribute("height");
-
-    // get base64 of svg file
-    fs.write(inputFile.replace(/\.svg$/i, ".txt"), svgdatauri + btoa(svgdata));
-
-    // set page viewport size to svg dimensions
-    page.viewportSize = {  width: parseFloat(width), height: parseFloat(height) };
-
-    // open svg file in webkit to make a png
-    page.open(inputFile, function(status)
+module.exports = function(grunt)
+{
+    grunt.registerMultiTask('rasterize', 'Convert SVG to PNG.', function()
     {
-        page.render(outputFile);
-        onComplete();
+        var next,
+            update,
+            start    = new Date(),
+            progress = 0,
+            complete = 0,
+            maxspawn = 10,
+            files    = grunt.file.expand(this.data.files),
+            done     = this.async();
+
+        grunt.log.subhead('SVG to PNG (' + files.length + ' files)...');
+
+        var onComplete = function()
+        {
+            if (++complete >= files.length) {
+                update();
+                grunt.log.write("\n");
+                grunt.log.ok("Rasterization complete.");
+                done();
+            } else {
+                update();
+                if (progress < files.length) next();
+            }
+        };
+
+        update = function()
+        {
+            process.stdout.clearLine();
+            process.stdout.cursorTo(0);
+
+            var str = style('0%', 'yellow') + ' [ ',
+                arr = [],
+                len = files.length,
+                percent = ((100/len)*(complete)).toFixed(2);
+
+            while(len--) arr.push(len < complete ? '=' : ' ');
+            str += arr.reverse().join('');
+            str += ' ] ' + style(percent + "%", 'green') + ' (' + ((new Date() - start)/1000).toFixed(1) + 's) ';
+            process.stdout.write(str);
+        };
+
+        next = function()
+        {
+            var inputFile = files[progress++],
+                outputFile = (inputFile || "").replace(/\.svg$/i, ".png");
+
+            if (!inputFile) {
+                return;
+            }
+
+            grunt.util.spawn(
+            {
+                cmd: 'phantomjs',
+                args: [
+                        'tasks/lib/svg2png.js',
+                        inputFile,
+                        outputFile
+                    ]
+                },
+                function(error, result, code)
+                {
+                    onComplete();
+                }
+            );
+        };
+
+        update();
+        var i = maxspawn;
+        while(--i) next();
+
     });
+
 };
-
-
